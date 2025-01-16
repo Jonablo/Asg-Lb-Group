1111bfd0bbdc6be059eb915d1819d7ad9652b0b4
Jonablo <jonasanchez1993@gmail.com>
2025-01-16 02:04:36 +0000

    Update endpoint display for minimal change.

--- RestApi_RyM/src/routes/index.js
+++ RestApi_RyM/src/routes/index.js
@@ -1,21 +1,21 @@
 const { Router } = require("express");
 const router = Router();
 
-router.get("/", (req, res) => {
-  res.render("index", { title: "Hello World", data: [] });
-});
+// router.get("/", (req, res) => {
+//   res.render("index", { title: "Hello World", data: [] });
+// });
 
-router.get("/test", (req, res) => {
-  const data = {
-    name: "Pablo",
-    semestre: "Octavo",
-  };
-  res.render("index", { title: "Test Data", data });
-});
+// router.get("/test", (req, res) => {
+//   const data = {
+//     name: "Pablo",
+//     semestre: "Octavo",
+//   };
+//   res.render("index", { title: "Test Data", data });
+// });
 
-const adress = require("../sample.json");
+// const adress = require("../sample.json");
 
-router.get("/RyM", async (req, res) => {
+router.get("/", async (req, res) => {
   const response = await fetch("https://rickandmortyapi.com/api/character");
   const data = await response.json();
   res.render("index", {
@@ -29,12 +29,12 @@ router.get("/RyM", async (req, res) => {
   });
 });
 
-router.get("/address", (req, res) => {
-  const formattedAdress = adress.map((entry) => ({
-    name: entry.name,
-    address: entry.address,
-  }));
-  res.render("index", { title: "Dataset-Servidor", data: formattedAdress });
-});
+// router.get("/address", (req, res) => {
+//   const formattedAdress = adress.map((entry) => ({
+//     name: entry.name,
+//     address: entry.address,
+//   }));
+//   res.render("index", { title: "Dataset-Servidor", data: formattedAdress });
+// });
 
 module.exports = router;
