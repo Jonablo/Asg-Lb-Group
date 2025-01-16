7c0be1446aa2f5b20854c7a1fe34c4b253b16372
Jonablo <jonasanchez1993@gmail.com>
2025-01-16 01:56:04 +0000

    Update endpoint display for minimal change.

--- RestApi_Address/src/index.js
+++ RestApi_Address/src/index.js
@@ -13,7 +13,7 @@ app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.use('/RestApi', require('./routes/index'));
+app.use('/', require('./routes/index'));
 
 app.listen(app.get('port'), () => {
     console.log(`Server on port ${app.get('port')}`);
