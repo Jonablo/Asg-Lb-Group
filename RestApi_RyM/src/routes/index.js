3f45a33738defac541482c145b67d0678958d75d
Jonablo <jonasanchez1993@gmail.com>
2025-01-16 04:27:23 +0000

    Update endpoint display for minimal change.

--- RestApi_RyM/src/routes/index.js
+++ RestApi_RyM/src/routes/index.js
@@ -5,29 +5,29 @@ const router = Router();
 //   res.render("index", { title: "Hello World", data: [] });
 // });
 
-// router.get("/test", (req, res) => {
-//   const data = {
-//     name: "Pablo",
-//     semestre: "Octavo",
-//   };
-//   res.render("index", { title: "Test Data", data });
-// });
+router.get("/", (req, res) => {
+  const data = {
+    name: "Pablo",
+    semestre: "Octavo",
+  };
+  res.render("index", { title: "Test Data", data });
+});
 
 // const adress = require("../sample.json");
 
-router.get("/", async (req, res) => {
-  const response = await fetch("https://rickandmortyapi.com/api/character");
-  const data = await response.json();
-  res.render("index", {
-    title: "Rick and Morty",
-    data: data.results.map((character) => ({
-      id: character.id,
-      name: character.name,
-      species: character.species,
-      image: character.image,
-    })),
-  });
-});
+// router.get("/", async (req, res) => {
+//   const response = await fetch("https://rickandmortyapi.com/api/character");
+//   const data = await response.json();
+//   res.render("index", {
+//     title: "Rick and Morty",
+//     data: data.results.map((character) => ({
+//       id: character.id,
+//       name: character.name,
+//       species: character.species,
+//       image: character.image,
+//     })),
+//   });
+// });
 
 // router.get("/address", (req, res) => {
 //   const formattedAdress = adress.map((entry) => ({
